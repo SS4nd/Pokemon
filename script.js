c52bc7d6661466a1aa61e11eae2005e9fe270892
SS4nd <silas.sandager2@gmail.com>
2023-03-27 16:15:32 +0200

    done

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
 "use strict";
-window.addEventListener("load",initApp);
 
+window.addEventListener("load", initApp);
 
-function initApp(){
-    const names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Hank', 'Ivy', 'Julia'];
+async function initApp() {
+  const grid = document.getElementById('grid');
+  const itemDetails = document.getElementById('item-details');
+  const itemText = document.getElementById('item-text');
+  var image = document.getElementById("curImage");
+  var popup = document.getElementById("popup");
+  var mainPop = document.getElementById("mainPop");
+  var img = document.getElementById("popupimg");
+  var foot = document.getElementById("popupfoot");
 
-    const grid = document.getElementById('grid');
-    
-    names.forEach(name => {
+  await fetch('https://cederdorff.github.io/dat-js/05-data/pokemons.json')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(name => {
       const listItem = document.createElement('li');
       listItem.className = 'grid-item';
-      
       const heading = document.createElement('h2');
-      heading.textContent = name;
+      heading.textContent = name.name;
       
+      listItem.addEventListener('mouseover', () => {
+        itemDetails.style.display = 'block';
+        itemText.textContent = name.dexindex +" "+ name.name[0].toUpperCase() + name.name.substring(1);
+        image.src = name.image;
+      });
+      
+      listItem.addEventListener('mouseout', () => {
+        itemDetails.style.display = 'none';
+      });
+      
+      listItem.addEventListener('click', () => {
+        mainPop.style.display = 'block';
+        itemDetails.style.display = 'block';
+        img.src = name.image;
+        const temp = `${name.description}<br>Abilities: ${name.ability}<br>Type and Subtype: ${name.type} ${name.subtype}<br>Weaknesses: ${name.weaknesses}<br>Gender: ${name.gender}<br>Weight(g):  ${name.weight} and Height(cm): ${name.height} <br>Generation:  ${name.generation}  <br>Game version:  ${name.spilversion} <br>Can evolve: ${name.canEvolve}<br>statsHP: ${name.statsHP}    statsAttack: ${name.statsAttack}  <br>statsDefence: ${name.statsDefence}   statsSpeed: ${name.statsSpeed}   <br>statsSpecialDefence: ${name.statsSpecialDefence}   statsSpecialAttack: ${name.statsSpecialAttack}`;        
+        
+        document.getElementById("popuph").innerHTML = name.dexindex +" "+ name.name[0].toUpperCase() + name.name.substring(1);
+        document.getElementById("popupp").innerHTML = temp;
+        foot.src = name.footprint;
+        popup.addEventListener('mouseout', () => {
+          mainPop.style.display = 'none';
+        });
+      });
+  
       listItem.appendChild(heading);
-      
       grid.appendChild(listItem);
     });
-    
+  })
+  .catch(error => {
+    console.error('Error fetching JSON: ', error);
+  });
 }
+
